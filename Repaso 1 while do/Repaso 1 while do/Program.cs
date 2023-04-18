695c029bfd184f521e01a3bf07a06b7f0fb7cb69
Ferrerx46 <mateo.ferrero.ices@gmail.com>
2023-04-18 19:11:23 -0300

    primer comit

---
+++ Repaso 1 while do/Repaso 1 while do/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+NewMethod();
+
+static void NewMethod()
+{
+    // Ejercicio 1
+    /*
+    Console.WriteLine("Ingrese la clave");
+    string? clave;
+
+    do
+    {
+        clave = Console.ReadLine();
+    } while (clave != "667788");
+    
+    Console.WriteLine("Contraseña correcta");
+    */
+
+    // Ejercicio 2
+    /*
+    int numero;
+     
+    do
+    {
+        Console.WriteLine("Ingrese un numero");
+        numero = int.Parse(Console.ReadLine());
+        
+    } while (numero <= 0);
+    
+    Console.WriteLine("El numero ingresado es: " + numero);
+    */
+    // Ejercicio 3
+
+    int numero;
+    int negativos = 0;
+    int sumaneg = 0;
+    int promedio = 0;
+    do
+    {
+        Console.Write("Ingrese un numero: ");
+        numero = int.Parse(Console.ReadLine());
+
+        if (numero < 0) ;
+        {
+            negativos++;
+            sumaneg += numero;
+        }
+    } while (numero != 0);
+
+    if (negativos > 0)
+    {
+        promedio = sumaneg / negativos;
+    }
+
+    Console.WriteLine("El numero ingresado es: " + numero);
+    Console.WriteLine("Su promedio es: " + promedio);
+
+
+    // Ejercicio 4
+    /*
+     int numero2;
+     do
+     {
+         Console.WriteLine("Ingrese un numero");
+         numero2 = int.Parse(Console.ReadLine());
+     } while (numero2 <= 99);
+
+     Console.WriteLine("El numero ingresado es: " + numero2);
+     */
+
+
+}
